6bc8046c14f7e12582a52ea016be5b5f7d03c167
Asher <asher.christian@gmail.com>
2025-03-08 13:48:12 -0800

    CSHW6

--- CS180/HW6/19-329.py
+++ CS180/HW6/19-329.py
@@ -1,13 +1,25 @@
-x = "101"
-y = "00"
-s = "100010101"
+import numpy as np
+x = "10111"
+i = len(x)
+y = "0100"
+j=len(y)
+s = "100101110100101110100101110100101110100101110100101110100101110101111011110111"
+k = len(s)
 
-val = [[-1 for i in range(len(s) )], [-1 for i in range(len(s))]]
-val[0][0] = 0 if x[0] == s[0] else -1
-val[1][0] = 0 if y[0] == s[0] else -1
-print(val)
+opt = np.zeros((k,i,j))
 
-for i, c in enumerate(s):
-    if i == 0:
-        continue
 
+for q in range(k):
+    for a in range(i):
+        for b in range(j):
+            if q == 0:
+                if a == 0 and b == 1:
+                    opt[0][a][b] = y[0] == s[0]
+                elif a == 1 and b == 0:
+                    opt[0][a][b] = x[0] == s[0]
+                else:
+                    opt[0][a][b] = False
+            else:
+                opt[q][a][b] = (opt[q-1][a-1][b] and s[q] == x[a-1]) or (opt[q-1][a][b-1]
+                                    and s[q] == y[b-1])
+print(opt[k-1].any())
